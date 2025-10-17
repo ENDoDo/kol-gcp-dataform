a89bea8b6b180da51abd3462d7c70c00c2100f82
ENDoDo <t.endo0521@gmail.com>
2025-10-17 23:17:15 +0900

    変更差分に対してコミット文を作成して

---
+++ definitions/sources/kol_sources.js
@@ -0,0 +1,22 @@
+// definitions/sources/kol_sources.js
+
+// kol_keibaデータセット内の全てのソーステーブルをここで宣言します
+declare({
+  schema: "kol_keiba",
+  name: "kol_den1"
+});
+
+declare({
+  schema: "kol_keiba",
+  name: "kol_den2"
+});
+
+declare({
+  schema: "kol_keiba",
+  name: "kol_sei1"
+});
+
+declare({
+  schema: "kol_keiba",
+  name: "kol_sei2"
+});
