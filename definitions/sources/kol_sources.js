fa61c09da138d95e6c37068d024e7fc5033d4f78
ENDoDo <t.endo0521@gmail.com>
2025-10-21 00:57:49 +0900

    データセット名を変更

--- definitions/sources/kol_sources.js
+++ definitions/sources/kol_sources.js
@@ -2,21 +2,21 @@
 
 // kol_keibaデータセット内の全てのソーステーブルをここで宣言します
 declare({
-  schema: "kol_keiba",
+  schema: "kolbi_keiba",
   name: "kol_den1"
 });
 
 declare({
-  schema: "kol_keiba",
+  schema: "kolbi_keiba",
   name: "kol_den2"
 });
 
 declare({
-  schema: "kol_keiba",
+  schema: "kolbi_keiba",
   name: "kol_sei1"
 });
 
 declare({
-  schema: "kol_keiba",
+  schema: "kolbi_keiba",
   name: "kol_sei2"
 });
