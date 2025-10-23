3f958b91887767ed221f4559ff6fbeff3f292e3d
ENDoDo <t.endo0521@gmail.com>
2025-10-24 02:07:39 +0900

    STG化の修正

--- definitions/sources/kol_sources.js
+++ definitions/sources/kol_sources.js
@@ -1,28 +1,31 @@
 // definitions/sources/kol_sources.js
-const { source_schema } = dataform.projectConfig.vars;
+const { source_schema, source_schema_stg } = dataform.projectConfig.vars;
+const defaultSchema = dataform.projectConfig.defaultSchema;
+
+const schema = defaultSchema === 'kolbi_analysis_stg' ? source_schema_stg : source_schema;
 
 // kolbi_keiba
 declare({
-  schema: source_schema,
+  schema: schema,
   name: "kol_den1"
 });
 
 declare({
-  schema: source_schema,
+  schema: schema,
   name: "kol_den2"
 });
 
 declare({
-  schema: source_schema,
+  schema: schema,
   name: "kol_sei1"
 });
 
 declare({
-  schema: source_schema,
+  schema: schema,
   name: "kol_sei2"
 });
 
 declare({
-  schema: source_schema,
+  schema: schema,
   name: "kol_ket"
 });
