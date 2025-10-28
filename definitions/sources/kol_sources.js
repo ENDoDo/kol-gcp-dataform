a542f4e0d9b06ecfcb2a9a5b10334888728b4ab9
ENDoDo <t.endo0521@gmail.com>
2025-10-28 19:20:13 +0900

    kol_sources.jsの修正

--- definitions/sources/kol_sources.js
+++ definitions/sources/kol_sources.js
@@ -1,31 +1,31 @@
 // definitions/sources/kol_sources.js
-const { source_schema, source_schema_stg } = dataform.projectConfig.vars;
-const defaultSchema = dataform.projectConfig.defaultSchema;
 
-const schema = defaultSchema === 'kolbi_analysis_stg' ? source_schema_stg : source_schema;
+// Terraformのrelease_configから渡される 'source_schema' 変数を直接使用します。
+// これにより、実行環境（prd/stg）に応じて適切なソーススキーマ（kolbi_keiba/kolbi_keiba_stg）が動的に選択されます。
+const { source_schema } = dataform.projectConfig.vars;
 
 // kolbi_keiba
 declare({
-  schema: schema,
+  schema: source_schema,
   name: "kol_den1"
 });
 
 declare({
-  schema: schema,
+  schema: source_schema,
   name: "kol_den2"
 });
 
 declare({
-  schema: schema,
+  schema: source_schema,
   name: "kol_sei1"
 });
 
 declare({
-  schema: schema,
+  schema: source_schema,
   name: "kol_sei2"
 });
 
 declare({
-  schema: schema,
+  schema: source_schema,
   name: "kol_ket"
 });
